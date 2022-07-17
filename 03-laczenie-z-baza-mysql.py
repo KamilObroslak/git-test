aaa8f753f75c54bc5dc95fc7a054962a6f9f7f43
Kamil Obroslak <kamilobroslak1@gmail.com>
2022-07-17 12:49:11 +0200

    Pierwsze polaczenie z bd xampp ksiegarnia

---
+++ 03-laczenie-z-baza-mysql.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+     host="localhost",
+     user="root",
+     passwd='',
+     database="ksiegarnia"
+)
+
+if mydb.is_connected():
+     print("Successfully Connected")
+else:
+     print("Unsuccessfull Connected")
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM klienci where idklienta %2 = 0")
+
+myresult = mycursor.fetchall()
+
+for row in myresult:
+     print(row)
